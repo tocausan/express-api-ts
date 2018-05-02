594cf47c5306a0ce9c1efaca02f046713fac68ef
tocausan <tocausan@gmail.com>
2018-05-02 22:34:35 +0200

    CORS option enable

--- src/middlewares/header.ts
+++ src/middlewares/header.ts
@@ -5,6 +5,6 @@ export const HeaderMiddleware = {
         res.header('Access-Control-Allow-Origin', "*");
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
-        return next();
+        return 'OPTIONS' === req.method ? res.sendStatus(200) : next();
     }
 };
